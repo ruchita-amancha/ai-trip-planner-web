b2c60a2ff266bf51396d93adba36b29765049c76
Ruchita Amancha <amancharuchita@gmail.com>
2024-11-11 09:20:37 +0530

    import path changed

--- src/view-trip/[tripId]/index.jsx
+++ src/view-trip/[tripId]/index.jsx
@@ -2,7 +2,7 @@ import { db } from '@/service/firebaseConfig';
 import { doc, getDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import InfoSection from '../components/infoSection';
+import InfoSection from '../components/InfoSection';
 import Hotels from '../components/Hotels';
 import PlacesToVisit from '../components/PlacesToVisit';
 import Footer from '../components/Footer';
