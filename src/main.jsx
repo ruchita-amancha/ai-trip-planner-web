979538219ebc11c7079de274278db536e60d2e0d
Ruchita Amancha <amancharuchita@gmail.com>
2024-11-17 12:55:32 +0530

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider, Routes } from 'react-router-dom'
 import CreateTrip from './create-trip'
 import Header from './components/custom/header'
 import { Toaster } from './components/ui/toaster'
@@ -38,7 +38,10 @@ createRoot(document.getElementById('root')).render(
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
     <Header></Header>
     <Toaster/>
-    <RouterProvider router={router}/>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-trip" element={<CreateTrip />} />
+      </Routes>
     </GoogleOAuthProvider>
   </StrictMode>,
 )
