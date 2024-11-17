e31d5c31cc8fc7f1112f09b48c6adc9d9cb8c788
Ruchita Amancha <amancharuchita@gmail.com>
2024-11-17 13:25:15 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -34,14 +34,12 @@ const router=createBrowserRouter([
 ])
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+    <StrictMode>
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
-    <Header></Header>
-    <Toaster/>
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-trip" element={<CreateTrip />} />
-      </Routes>
+      <Header />
+      <Toaster />
+      {/* Use RouterProvider with the defined router */}
+      <RouterProvider router={router} />
     </GoogleOAuthProvider>
-  </StrictMode>,
+  </StrictMode>
 )
